46f1953869761d1f1a040596a6d27a246682e7c0
sitahlatif <sitah27@hotmail.com>
2019-02-14 15:55:06 +0300

    car-market

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Car
+from .forms import CarForm
+from django.contrib import messages
+
 
 def car_list(request):
 	cars = Car.objects.all()
@@ -18,15 +21,37 @@ def car_detail(request, car_id):
 
 
 def car_create(request):
-	#Complete Me
-	return render(...)
+	form = CarForm()
+	if request.method == "POST":
+		form = CarForm(request.POST , request.FILES or None)
+		if form.is_valid():
+			form.save()
+			messages.success(request, 'created.')
+			return redirect('car-list')
+	context = {
+		"form":form,
+	}
+	return render(request, 'create.html', context)
 
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
+	car = Car.objects.get(id = car_id)
+	form = CarForm(instance = car)
+	if request.method == "POST":
+		form = CarForm(request.POST, request.FILES or None, instance = car)
+		if form.is_valid():
+			form.save()
+			messages.success(request, 'updated.')
+			return redirect('car-list')
+	context = {
+		"form":form,
+		"car": car
+	}
+	return render(request, 'update.html', context)
+
 
 
 def car_delete(request, car_id):
-	#Complete Me
-	return render(...)
+	Car.objects.get(id = car_id).delete()
+	messages.success(request, 'deleted.')
+	return redirect('car-list')
